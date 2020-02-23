11613625ed0974b4f087adbacf116f65f49ef271
Gokuuu- <60169560+fatahkbm123@users.noreply.github.com>
2020-02-23 20:03:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ function myFunction (x) {
    body.classList.toggle('active')
 }
 
-const tl = gsap.timeline({default: {duration: 1}});
+const tl = gsap.timeline();
    tl
+   .from('nav', 3, {y: -2500, opacity: 0}, '-=1.5')
    .from('.myFoto', 5, {x: -1000, opacity: 0, ease: "elastic.out(1, 0.3)"})
    .from('.bulat', 5, {x: 1000, opacity: 0, ease: "elastic.out(1, 0.3)"}, '-=5.5')
-   .from('.anim1', 1, {y: -20, opacity: 0, stagger: .4, scaleY: -1},'-=4.5')
+   .from('.anim1', 1, {y: -20, opacity: 0, stagger: .4, scaleY: -1},'-=2.5')
    .from('.anim2', {y: 20, opacity: 0, stagger: .4, scaleX: -1}, '-=3')
    .from('.nama', {y: 20, opacity: 0}, '-=2.5')
    .from('.navbar-nav a', {opacity: 0, x: 20, stagger: .4, scaleY: -1}, '-=2')
    .from('.borderRight', {opacity: 0, width: 0, height: 0, duration: 1.2}, '-=4')
+
 
