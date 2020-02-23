ae5668c8159a072adcc9e26f5bb46bb0f1843d55
Gokuuu- <60169560+fatahkbm123@users.noreply.github.com>
2020-02-23 20:18:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function myFunction (x) {
 
 const tl = gsap.timeline({default: {duration: 1}});
    tl
-    .from('nav', 5, {y: -20000, opacity: 0}, '-=3')
+   .from('nav', {y: -1000, opacity: 0, duration: 5}, '-=2')
    .from('.myFoto', 5, {x: -1000, opacity: 0, ease: "elastic.out(1, 0.3)"})
    .from('.bulat', 5, {x: 1000, opacity: 0, ease: "elastic.out(1, 0.3)"}, '-=5.5')
    .from('.anim1', 1, {y: -20, opacity: 0, stagger: .4, scaleY: -1},'-=2.5')
@@ -15,5 +15,4 @@ const tl = gsap.timeline({default: {duration: 1}});
    .from('.nama', {y: 20, opacity: 0}, '-=2.5')
    .from('.navbar-nav a', {opacity: 0, x: 20, stagger: .4, scaleY: -1}, '-=2')
    .from('.borderRight', {opacity: 0, width: 0, height: 0, duration: 1.2}, '-=4')
-
 
