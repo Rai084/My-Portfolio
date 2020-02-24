10e32c8f2c0104655d5945cefe9add8398088e55
Gokuuu- <60169560+fatahkbm123@users.noreply.github.com>
2020-02-24 19:09:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const tl = gsap.timeline({default: {duration: 1}});
    .to('.kotak4', {opacity: 0, y: 100, duration: 1}, '-=.5')
    .to('.kotak5', {opacity: 0, y: -100, duration: 1}, '-=.5')
    .to('.loader-wrapper', {opacity: 1, x: -1500, duration: 3})
-   .to('nav', {y: 0, opacity: 1, duration: 1.5}, '-=2')
+   .to('nav', {y: 0, opacity: 1, duration: 1.2}, '-=2.5')
    .from('.myFoto', {x: -1000, opacity: 0,duration: 4, ease: "elastic.out(1, 0.3)"})
    .from('.bulat', {opacity: 0, x: 1000, duration: 4, ease: "elastic.out(1, 0.3)"}, '-=3')
    .from('.borderRight', {opacity: 0, width: 0, height: 0, duration: 2}, '-=4')
@@ -21,4 +21,3 @@ const tl = gsap.timeline({default: {duration: 1}});
    .from('.nama', {y: 20, opacity: 0}, '-=2.5')
    .from('.navbar-nav a', {opacity: 0, x: 20, stagger: .4, scaleY: -1}, '-=2')
    .from('button', {x: 20, opacity: 0}, '-=2.5')
-   
