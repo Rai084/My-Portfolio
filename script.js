f8c3351d63c85120d86f9908f40252936f064f8d
Gokuuu- <60169560+fatahkbm123@users.noreply.github.com>
2020-02-27 10:39:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,81 @@ const tl = gsap.timeline({default: {duration: 1}});
    .from('.nama', {y: 20, opacity: 0}, '-=2.5')
    .from('.navbar-nav a', {opacity: 0, x: 20, stagger: .4, scaleY: -1}, '-=2')
    .from('button', {x: 20, opacity: 0}, '-=2.5')
+
+$('.kotak6, .description').on('mouseover', function() {
+   $('.kotak6').css({
+      'transform': 'rotate(0deg)',
+      'borderRadius': '50%',
+      'backgroundColor': '#1641ff'
+   })
+   // $('.kotak').addClass('animateKotak');
+
+   $('.description').css({
+      'color': 'white'
+   })
+})
+
+$('.kotak6').on('mouseout', function() {
+   $('.kotak6').css({
+      'transform': 'rotate(45deg)',
+      'borderRadius': '4px',
+      'border': '4px solid #1641ff',
+      'backgroundColor': '#fff'
+   })
+
+   $('.description').css({
+      'color': ''
+   })
+})
+
+$('.kotak7, .description2').on('mouseover', function() {
+   $('.kotak7').css({
+      'transform': 'rotate(0deg)',
+      'borderRadius': '50%',
+      'backgroundColor': '#1641ff'
+   })
+   $('.kotak').addClass('animateKotak');
+
+   $('.description2').css({
+      'color': 'white'
+   })
+})
+
+$('.kotak7').on('mouseout', function() {
+   $('.kotak7').css({
+      'transform': 'rotate(45deg)',
+      'borderRadius': '4px',
+      'border': '4px solid #1641ff',
+      'backgroundColor': '#fff'
+   })
+
+   $('.description2').css({
+      'color': ''
+   })
+})
+
+$('.kotak8, .description3').on('mouseover', function() {
+   $('.kotak8').css({
+      'transform': 'rotate(0deg)',
+      'borderRadius': '50%',
+      'backgroundColor': '#1641ff'
+   })
+   $('.kotak').addClass('animateKotak');
+
+   $('.description3').css({
+      'color': 'white'
+   })
+})
+
+$('.kotak8').on('mouseout', function() {
+   $('.kotak8').css({
+      'transform': 'rotate(45deg)',
+      'borderRadius': '4px',
+      'border': '4px solid #1641ff',
+      'backgroundColor': '#fff'
+   })
+
+   $('.description3').css({
+      'color': ''
+   })
+})
